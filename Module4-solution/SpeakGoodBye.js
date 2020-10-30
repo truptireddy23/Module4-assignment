e08cad15c131109b7a002268a00a29f2dcd371ae
Trupti Reddy <67378670+truptireddy23@users.noreply.github.com>
2020-10-30 19:33:48 +0530

    Add files via upload

---
+++ Module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function() {
+    var byeSpeaker={};
+    var speakWord = "Good bye";
+    byeSpeaker.speak=function (name) {
+    console.log(speakWord + " " + name);
+    }
+    window.byeSpeaker=byeSpeaker;
+  })(window);
